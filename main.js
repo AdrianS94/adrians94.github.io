72b780a5a56d1b1a99712bf7cb72ff4757313425
AdrianS94 <sadrianmihai94@gmail.com>
2021-01-21 13:53:58 +0200

    update style

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ fetch("https://tribustest.free.beeceptor.com/api/people")
     const content = data.map((item) => {
       if (item.gender === "Male") {
         return `<div class="card-male">
+                        <img src="male.png" alt="">
                         <h2>First Name: ${item.first_name}</h2>
                         <h2>Last Name: ${item.last_name}</h2>
-                        <p>email: ${item.email}</p>
-                        <p>gender: ${item.gender}</p>
+                        <p><i class="far fa-envelope"></i>email: ${item.email}</p>
+                        <p>gender: ${item.gender}<i class="fas fa-male"></i></p>
                     </div>`;
       } else if (item.gender === "Female") {
         return `<div class="card-female">
+                        <img src="female.png" alt="">
                         <h2>First Name: ${item.first_name}</h2>
                         <h2>Last Name: ${item.last_name}</h2>
-                        <p>email: ${item.email}</p>
-                        <p>gender: ${item.gender}</p>
+                        <p><i class="far fa-envelope"></i>email: ${item.email}</p>
+                        <p>gender: ${item.gender}<i class="fas fa-female"></i></p>
                     </div>`;
       }
     });
